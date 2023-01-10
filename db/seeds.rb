8f1eaaab96f235e9c1cbf3511c7ca49622c81131
washonrails <iamx.3ur@protonmail.com>
2023-01-10 19:33:49 -0300

    bulma framework added -> Navbar in dev

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# 10.times do |_|
+#   user_name = Faker::Name.name
+#   user_email = Faker::Internet.email
+#   user = "#{user_name} - #{user_email}"
+
+#   puts "Cadastrando o usuario #{user}"
+#   User.create(name: user_name, email: user_email)
+# end
